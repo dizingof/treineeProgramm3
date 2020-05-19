a5d6b0efce4076f3924266f50b1de30ad27eb2d4
Roman.Nepomniashchyi <Roman.Nepomniashchyi@infopulse.com>
2020-05-19 22:37:09 +0300

    add table command

--- AdoConnectionString/AdoCommands/Program.cs
+++ AdoConnectionString/AdoCommands/Program.cs
@@ -11,6 +11,7 @@ namespace AdoCommands
     {
         static void Main(string[] args)
         {
+            /*
             string connectionString = @"Data Source=(localdb)\MSSQLLocalDB; Initial Catalog=ShopNew; Integrated Security=True";
             SqlConnection connection = new SqlConnection(connectionString);
             connection.Open();
@@ -18,7 +19,23 @@ namespace AdoCommands
             string motorType = (string)cmd.ExecuteScalar();
             Console.WriteLine(motorType);
             Console.ReadLine();
-
+            */
+            string connectionString = @"Data Source=(localdb)\MSSQLLocalDB; Initial Catalog=ShopNew; Integrated Security=True";
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            SqlCommand cmd = new SqlCommand("SELECT  *  FROM[ShopNew].[dbo].[Cars]", connection);
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    Console.WriteLine(reader.GetName(i) + ": " + reader[i]);
+                }
+                Console.WriteLine(new string('_', 20));
+            }
+            reader.Close();
+            connection.Close();
+            Console.ReadLine();
 
         }
     }
