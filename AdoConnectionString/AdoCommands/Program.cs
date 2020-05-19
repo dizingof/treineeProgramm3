218ac1bb112a6aa63d3ead930252c3ea70cdd8fc
Roman.Nepomniashchyi <Roman.Nepomniashchyi@infopulse.com>
2020-05-19 22:18:52 +0300

    add solution ado command and implement first command

---
+++ AdoConnectionString/AdoCommands/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoCommands
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = @"Data Source=(localdb)\MSSQLLocalDB; Initial Catalog=ShopNew; Integrated Security=True";
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            SqlCommand cmd = new SqlCommand("SELECT  [MotorType]  FROM[ShopNew].[dbo].[Cars]  where CarId = '10001'", connection);
+            string motorType = (string)cmd.ExecuteScalar();
+            Console.WriteLine(motorType);
+            Console.ReadLine();
+
+
+        }
+    }
+}
