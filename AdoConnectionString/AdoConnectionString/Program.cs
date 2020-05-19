106b76e6286b681e59208c0c65137dcd8230feba
Roman.Nepomniashchyi <Roman.Nepomniashchyi@infopulse.com>
2020-05-19 20:22:59 +0300

    create connect instance

--- AdoConnectionString/AdoConnectionString/Program.cs
+++ AdoConnectionString/AdoConnectionString/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,22 @@ namespace AdoConnectionString
     {
         static void Main(string[] args)
         {
-           
+            string connectionString = @"Data Source=(localdb)\MSSQLLocalDB; Initial Catalog=ShopNew; Integrated Security=True";
+            SqlConnection connection = new SqlConnection(connectionString);
+            try
+            {
+                connection.Open();
+                Console.WriteLine(connection.State);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally 
+            {
+                connection.Close();
+                Console.WriteLine(connection.State);
+            }
         }
     }
 }
