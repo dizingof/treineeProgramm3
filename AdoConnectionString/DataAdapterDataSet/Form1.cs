b66479908930f5e2925872bd20d3386e1d00b3bb
Roman.Nepomniashchyi <Roman.Nepomniashchyi@infopulse.com>
2020-05-21 23:10:51 +0300

    add edit DB

--- AdoConnectionString/DataAdapterDataSet/Form1.cs
+++ AdoConnectionString/DataAdapterDataSet/Form1.cs
@@ -13,28 +13,73 @@ namespace DataAdapterDataSet
 {
     public partial class Form1 : Form
     {
+        DataSet ds;
+        SqlDataAdapter adapter;
+        SqlCommandBuilder commandBuilder;
+        string connectionString = @"Data Source=(localdb)\MSSQLLocalDB; Initial Catalog=ShopNew; Integrated Security=True";
+        string sql = "SELECT * FROM[ShopNew].[dbo].[TestTable]";
         public Form1()
         {
             InitializeComponent();
-            string connectionString = @"Data Source=(localdb)\MSSQLLocalDB; Initial Catalog=ShopNew; Integrated Security=True";
-            string sql = "SELECT * FROM[ShopNew].[dbo].[Cars]";
+
+            SqlCommandBuilder commandBuilder;
+            
+
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView1.AllowUserToAddRows = false;
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
                 connection.Open();
                 // Создаем объект DataAdapter
-                SqlDataAdapter adapter = new SqlDataAdapter(sql, connection);
+                adapter = new SqlDataAdapter(sql, connection);
                 // Создаем объект Dataset
-                DataSet ds = new DataSet();
+                ds = new DataSet();
                 // Заполняем Dataset
                 adapter.Fill(ds);
                 // Отображаем данные
                 dataGridView1.DataSource = ds.Tables[0];
+                dataGridView1.Columns["Id"].ReadOnly = true;
             }
         }
+
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DataRow row = ds.Tables[0].NewRow(); // добавляем новую строку в DataTable
+            ds.Tables[0].Rows.Add(row);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // удаляем выделенные строки из dataGridView1
+            foreach (DataGridViewRow row in dataGridView1.SelectedRows)
+            {
+                dataGridView1.Rows.Remove(row);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                adapter = new SqlDataAdapter(sql, connection);
+                commandBuilder = new SqlCommandBuilder(adapter);
+                adapter.InsertCommand = new SqlCommand("sp_CreateUser", connection);
+                adapter.InsertCommand.CommandType = CommandType.StoredProcedure;
+                adapter.InsertCommand.Parameters.Add(new SqlParameter("@TextData", SqlDbType.NVarChar, 50, "TextData"));
+                
+
+                SqlParameter parameter = adapter.InsertCommand.Parameters.Add("@Id", SqlDbType.Int, 0, "Id");
+                parameter.Direction = ParameterDirection.Output;
+
+                adapter.Update(ds);
+            }
         }
     }
 }
